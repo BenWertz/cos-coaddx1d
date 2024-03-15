Added data quality flags to weight calculation, fixed coadd filenames, added diagnostic programs for viewing data quality

coadd_cos.py
@@ -89,12 +89,15 @@ line_lib=get_line_lib(target_lines,wavelength_range[arg_Grating.lower()])
 full_line_lib=get_line_lib(full_line_list,wavelength_range[arg_Grating.lower()])
 
 # Initial list of files to be read (some may be discarded if they don't have the right grating)
-filenames_prelim = glob.glob(arg_TgtFolder+"/*x1d.fits")
+filenames_prelim = glob.glob(arg_TgtFolder+"/**/*x1d.fits",recursive=True)
 
 filenames=[]
 spec_table={}
 for file in filenames_prelim:
     spec_data,spec_hdu=spectrum_align.read_x1d_file(file)
+    if spec_data["exptime"]==0:
+        continue
+    
     # Only accept gratings that match the mode
     if matchGrating(spec_data["grating"],arg_Grating):
         # utils.viewX1DFile(file)
@@ -171,14 +174,17 @@ velocity_table=QTable(
 
 TGT_LINE_NAME_LIST=line_lib["line_name"]
 
-os.chdir(sys.path[0])
-if not os.path.exists("outputs"):
-    os.mkdir("outputs")
-    os.mkdir("outputs/figures")
+os.chdir(arg_TgtFolder)
+if not os.path.exists("coadd_x1d"):
+    os.mkdir("coadd_x1d")
+figurePath=f"coadd_x1d/{arg_Grating}/figures"
+if not os.path.exists(figurePath):
+    os.mkdir(f"coadd_x1d/{arg_Grating}")
+    os.mkdir(figurePath)
 
 for line,wlen in zip(full_line_lib["line_name"],full_line_lib["lambda"]):
     diagnostic_plots.diagnostic_plot(
-        "outputs/figures",
+        figurePath,
         spec_table,line,wlen,output_table,
         filenames,
         line_lib,
@@ -200,20 +206,18 @@ coadd_data=coaddition.coadd_spec(
     arg_COADD_MODE,
     3
 )
+
+coaddition.save_spec_data(
+    f"coadd_x1d/{arg_Grating}",
+    coadd_data
+)
+coaddition.save_spec_data(
+    f"coadd_x1d/{arg_Grating}",
+    coadd_data_unbinned
+)
+
 ref_wave,coadded_flux,coadded_error=coadd_data["wave"],coadd_data["flux"],coadd_data["error"]
 ref_wave_unbinned,coadded_flux_unbinned,coadded_error_unbinned=coadd_data_unbinned["wave"],coadd_data_unbinned["flux"],coadd_data_unbinned["error"]
-
-# print(ref_wave[np.isnan(coadded_error)])
-# print("Min:")
-# print([spec_table[f]["header"]["MINWAVE"] for f in filenames])
-# print("Max:")
-# print([spec_table[f]["header"]["MAXWAVE"] for f in filenames])
-
-# print("Wave array min")
-# print([spec_table[f]["wave"].min().value for f in filenames])
-# print("Wave array max")
-# print([spec_table[f]["wave"].max().value for f in filenames])
-
 
 plt.figure(figsize=(12,6))
 lim=diagnostic_plots.plot_clip_lim(coadded_flux,5)
@@ -221,24 +225,18 @@ plt.ylim(lim[0],lim[1])
 
 plt.step(ref_wave,coadded_flux,color="k",lw=1,label=f"Coadded flux ({coadd_data['weighing_method']}, bins={coadd_data['binning']})")
 
-plt.step(ref_wave_unbinned,coadded_flux_unbinned,color="red",lw=0.5,alpha=0.7,label=f"Coadded flux ({coadd_data_unbinned['weighing_method']}, unbinned)")
+plt.step(ref_wave_unbinned,coadded_flux_unbinned,color="lightblue",lw=0.9,alpha=0.7,label=f"Coadded flux ({coadd_data_unbinned['weighing_method']}, unbinned)")
 
-plt.step(ref_wave,coadded_error,color="k",lw=1,alpha=0.5,label="Error (binned)")
+plt.step(ref_wave,coadded_error,color="k",lw=0.5,alpha=0.7,label="Error (binned)")
 
-plt.step(ref_wave_unbinned,coadded_error_unbinned,color="red",lw=1,alpha=0.5,label="Error (unbinned)")
+plt.step(ref_wave_unbinned,coadded_error_unbinned,color="orange",lw=0.5,alpha=0.5,label="Error (unbinned)")
 
 plt.legend(loc="upper left",fontsize="x-small")
 plt.xlabel("$\lambda (\AA)$")
 plt.ylabel("Flux (ergs s^-1 m^-2 sr^-1)")
 plt.minorticks_on()
-plt.savefig("outputs/figures/coadd.png")
-plt.show()
+plt.savefig(f"{figurePath}/coadd_spec.pdf")
 
-coaddition.save_spec_data(
-    "outputs/coadd",
-    coadd_data
-)
-coaddition.save_spec_data(
-    "outputs/coadd_unbinned",
-    coadd_data_unbinned
-)
+plt.draw()
+plt.pause(0.001)
+input("(press enter to close plots)")

coaddition.py
@@ -5,10 +5,12 @@ import spectrum_align
 import astropy.units as u
 from scipy.interpolate import *
 
-def save_spec_data(filename, spec_data): 
+def save_spec_data(filepath, spec_data): 
     # save a fits file with header information
     today = str(datetime.datetime.now())
     coadd_header=spec_data["header"]
+
+    filename=f'{filepath}/{coadd_header["TARGNAME"]}_{coadd_header["OPT_ELEM"].lower()}_coadd_x1d_bin{spec_data["binning"]}_{spec_data["weighing_method"]}'
 
     hdr = fits.Header()
     hdr['TELESCOP'] = coadd_header['TELESCOP']
@@ -26,12 +28,12 @@ def save_spec_data(filename, spec_data):
 
     colWav = fits.Column(name='WAVE', format='F', array=np.around(spec_data["wave"], decimals=4))
     colFlx = fits.Column(name='FLUX', format='F', array=spec_data["flux"])
-    # colErr = fits.Column(name='ERR', format='F', array=spec_data["error"])
+    colErr = fits.Column(name='ERROR', format='F', array=spec_data["error"])
 
     cols = fits.ColDefs([
         colWav,
         colFlx,
-        # colErr
+        colErr
     ])
     table_hdu = fits.BinTableHDU.from_columns(cols)
 
@@ -40,6 +42,58 @@ def save_spec_data(filename, spec_data):
     print('Saved ', filename+'.fits')
 
     return filename+'.fits'
+
+
+
+def weigh_data_quality(dq):
+    """
+    Reads the X1D's data quality array and looks for specific flags indicating
+    that a pixel should be thrown out or deweighted.
+
+    Returns an array of weights indicating if a pixel should be kept (weight of 1),
+    thrown out (weight of 0), or deweighted (weight of 0.5).
+    """
+
+    #      1 = 2^0  = Reed-Solomon error         --> throw out
+    #      2 = 2^1  = Not used                   --> ignore
+    #      4 = 2^2  = Detector shadow            --> de-weight
+    #      8 = 2^3  = Poorly calibrated          --> de-weight
+    #     16 = 2^4  = Very low response region   --> de-weight
+    #     32 = 2^5  = Background feature         --> de-weight
+    #     64 = 2^6  = Burst                      --> throw out
+    #    128 = 2^7  = Pixel out of bounds        --> throw out
+    #    256 = 2^8  = Fill data                  --> throw out
+    #    512 = 2^9  = Pulse height out of bounds --> throw out
+    #   1024 = 2^10 = Low response region        --> de-weight
+    #   2048 = 2^11 = Bad time interval          --> throw out
+    #   4096 = 2^12 = Low PHA feature            --> de-weight
+    #   8192 = 2^13 = Gain-sag hole              --> throw out
+    #  16384 = 2^14 = Not used                   --> ignore
+
+    dq_weights=np.ones(len(dq))
+
+    print(dq.dtype)
+
+    dq_weights[
+        ((dq & 2**0)>0) |
+        ((dq & 2**6)>0) |
+        ((dq & 2**7)>0) |
+        ((dq & 2**8)>0) |
+        ((dq & 2**9)>0) |
+        ((dq & 2**11)>0) |
+        ((dq & 2**13)>0)
+    ]=0
+
+    dq_weights[
+        ((dq & 2**2)>0) |
+        ((dq & 2**3)>0) |
+        ((dq & 2**4)>0) |
+        ((dq & 2**5)>0) |
+        ((dq & 2**10)>0) |
+        ((dq & 2**12)>0)
+    ]*=0.5
+
+    return dq_weights
 
 def coadd_spec(spec_table,filenames,offset_table,
         weighing="exptime",bin=1):
@@ -87,6 +141,9 @@ def coadd_spec(spec_table,filenames,offset_table,
         counts=spec_data["net"]
         flux=spec_data["flux"]
         error=spec_data["error"]
+        dq=spec_data["dq"]
+
+        dq_wgts=weigh_data_quality(dq)
 
         offset_wave=wave-offset_table["mean_dlambda"][i]
 
@@ -111,26 +168,56 @@ def coadd_spec(spec_table,filenames,offset_table,
             fill_value=0,
             bounds_error=False
         )
+        dq_wgts_interp=interp1d(
+            offset_wave,
+            dq_wgts,
+            kind="nearest",
+            fill_value=0,
+            bounds_error=False
+        )
 
         # spec_data["wave_updated"]=ref_wave
         interp_flux=interp(common_grid_wave)
         interp_net=net_interp(common_grid_wave)
         interp_err=err_interp(common_grid_wave)
+        interp_dq_wgt=dq_wgts_interp(common_grid_wave)
+
+        # Completely throw out invalid pixels
+        # Note: do I actually have to do this? It seems like setting the weight to zero should be enough
+        interp_flux[interp_dq_wgt==0]=0
+        interp_err[interp_dq_wgt==0]=0
+
+        # For deweighted datapoints, the error is multiplied by sqrt(2)
+        interp_err[interp_dq_wgt==0.5]*=(2**.5)
 
         if weighing=="exptime":
             weight=np.ones(len(interp_flux))*spec_data["exptime"]
         elif weighing=="modified_exptime":
             weight=(interp_net/interp_flux)*spec_data["exptime"]
             weight[interp_flux==0]=0.
+        
+        weight*=interp_dq_wgt
 
         mask=((interp_flux!=0) & ~np.isnan(interp_flux))
         coadded_flux[mask]+=interp_flux[mask]*weight[mask]
-        error_accum[mask]+=(interp_err[mask]**2)*weight[mask]
+
+        error_accum[mask]+=(interp_err[mask]**2)*weight[mask]**2
+
+        # error_accum[mask]+=(interp_err[mask]**2)*weight[mask]
 
         weights[mask]+=(mask.astype(np.float32)*weight)[mask]
 
     coadded_flux/=weights
-    coadded_error=(error_accum/weights)**.5
+    coadded_error=error_accum**.5/weights
+
+    # S=sqrt(N*(s^2*w^2))/(N*w)
+
+    # coadded_error=(error_accum/weights)**.5
+
+    # s^2=SUM(s_i^2*(w_i/W)^2)
+    # s^2=SUM(s_i^2*(w_i)^2)/W^2
+
+    # s=sqrt(sum(s_i^2*w_i^2))/W
 
     # coadded_flux=coadded_error
     # Binning
@@ -154,6 +241,12 @@ def bin_spec(x1, y1, error1, bins):
 
     dg_x1 = np.array([np.mean(x1.value[dg1==j]) for j in np.arange(len(b1)+1)[1:]])*x1.unit
     dg_y1 = np.array([np.mean(y1[dg1==j]) for j in np.arange(len(b1)+1)[1:]])
-    dg_error1 = np.array([np.mean(error1[dg1==j]**2)**.5 for j in np.arange(len(b1)+1)[1:]])
+    dg_error1 = np.array([np.sum(error1[dg1==j]**2)**.5/bins for j in np.arange(len(b1)+1)[1:]])
+    # dg_error1 = np.array([np.mean(error1[dg1==j]**2)**.5 for j in np.arange(len(b1)+1)[1:]])
+
+    # sqrt(a^2+a^2+a^2)/sqrt(3)
+    # sqrt(a^2+a^2+a^3)/sqrt(3)
+
+    # sqrt((s1**2+s2**2+s3**2)/3)
 
     return dg_x1, dg_y1, dg_error1